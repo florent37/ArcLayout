8afe3994104d216faf5f6c4bc451e412248d3bc2
Florent Champigny <florent.champigny@blablacar.com>
2016-11-23 10:47:43 +0100

    updated sample

--- app/src/main/java/com/github/florent37/sample/arclayout/MainActivity.java
+++ app/src/main/java/com/github/florent37/sample/arclayout/MainActivity.java
@@ -8,6 +8,6 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_main_inside);
     }
 }
