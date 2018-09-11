2844ae4eaab6a7f0bcfb356f2f9f8f6af69840fa
florent champigny <florent.champigny@backelite.com>
2018-09-11 16:17:07 +0200

    used shape of view implementation

--- arclayout/src/main/java/com/github/florent37/arclayout/ArcLayout.java
+++ arclayout/src/main/java/com/github/florent37/arclayout/ArcLayout.java
@@ -1,173 +1,168 @@
 package com.github.florent37.arclayout;
 
 import android.content.Context;
-import android.graphics.Canvas;
-import android.graphics.Color;
-import android.graphics.Outline;
-import android.graphics.Paint;
+import android.content.res.TypedArray;
 import android.graphics.Path;
-import android.graphics.PorterDuff;
-import android.graphics.PorterDuffXfermode;
-import android.os.Build;
-import android.support.annotation.RequiresApi;
-import android.support.v4.view.ViewCompat;
+import android.support.annotation.IntDef;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.util.AttributeSet;
-import android.view.View;
-import android.view.ViewOutlineProvider;
-import android.widget.FrameLayout;
 
-public class ArcLayout extends FrameLayout {
+import com.github.florent37.arclayout.manager.ClipPathManager;
 
-    private ArcLayoutSettings settings;
+public class ArcLayout extends ShapeOfView {
 
-    private int height = 0;
+    public static final int POSITION_BOTTOM = 1;
+    public static final int POSITION_TOP = 2;
+    public static final int POSITION_LEFT = 3;
+    public static final int POSITION_RIGHT = 4;
+    public static final int CROP_INSIDE = 1;
 
-    private int width = 0;
+    public static final int CROP_OUTSIDE = 2;
+    @ArcPosition
+    private int arcPosition = POSITION_TOP;
+    @CropDirection
+    private int cropDirection = CROP_INSIDE;
 
-    private Path clipPath;
+    private int arcHeight = 0;
 
-    public ArcLayout(Context context) {
+    public ArcLayout(@NonNull Context context) {
         super(context);
         init(context, null);
     }
 
-    public ArcLayout(Context context, AttributeSet attrs) {
+    public ArcLayout(@NonNull Context context, @Nullable AttributeSet attrs) {
         super(context, attrs);
         init(context, attrs);
     }
 
-    public void init(Context context, AttributeSet attrs) {
-        settings = new ArcLayoutSettings(context, attrs);
-        settings.setElevation(ViewCompat.getElevation(this));
-
-        /**
-         * If hardware acceleration is on (default from API 14), clipPath worked correctly
-         * from API 18.
-         *
-         * So we will disable hardware Acceleration if API < 18
-         *
-         * https://developer.android.com/guide/topics/graphics/hardware-accel.html#unsupported
-         * Section #Unsupported Drawing Operations
-         */
-        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.JELLY_BEAN_MR2) {
-            setLayerType(View.LAYER_TYPE_SOFTWARE, null);
-        }
+    public ArcLayout(@NonNull Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+        init(context, attrs);
     }
 
-    private Path createClipPath() {
-        final Path path = new Path();
-
-        float arcHeight = settings.getArcHeight();
-
-        switch (settings.getPosition()){
-            case ArcLayoutSettings.POSITION_BOTTOM:{
-                if (settings.isCropInside()) {
-                    path.moveTo(0, 0);
-                    path.lineTo(0, height);
-                    path.quadTo(width / 2, height - 2 * arcHeight, width, height);
-                    path.lineTo(width, 0);
-                    path.close();
-                } else {
-                    path.moveTo(0, 0);
-                    path.lineTo(0, height - arcHeight);
-                    path.quadTo(width / 2, height + arcHeight, width, height - arcHeight);
-                    path.lineTo(width, 0);
-                    path.close();
-                }
-                break;
-            }
-            case ArcLayoutSettings.POSITION_TOP:
-                if (settings.isCropInside()) {
-                    path.moveTo(0, height);
-                    path.lineTo(0, 0);
-                    path.quadTo(width / 2, 2 * arcHeight, width, 0);
-                    path.lineTo(width, height);
-                    path.close();
-                } else {
-                    path.moveTo(0, arcHeight);
-                    path.quadTo(width / 2, -arcHeight, width, arcHeight);
-                    path.lineTo(width, height);
-                    path.lineTo(0, height);
-                    path.close();
-                }
-                break;
-            case ArcLayoutSettings.POSITION_LEFT:
-                if (settings.isCropInside()) {
-                    path.moveTo(width, 0);
-                    path.lineTo(0, 0);
-                    path.quadTo(arcHeight * 2, height / 2, 0, height);
-                    path.lineTo(width, height);
-                    path.close();
-                } else {
-                    path.moveTo(width, 0);
-                    path.lineTo(arcHeight, 0);
-                    path.quadTo(-arcHeight, height / 2, arcHeight, height);
-                    path.lineTo(width, height);
-                    path.close();
-                }
-                break;
-            case ArcLayoutSettings.POSITION_RIGHT:
-                if (settings.isCropInside()) {
-                    path.moveTo(0, 0);
-                    path.lineTo(width, 0);
-                    path.quadTo(width - arcHeight * 2, height / 2, width, height);
-                    path.lineTo(0, height);
-                    path.close();
-                } else {
-                    path.moveTo(0, 0);
-                    path.lineTo(width - arcHeight, 0);
-                    path.quadTo(width + arcHeight, height / 2, width - arcHeight, height);
-                    path.lineTo(0, height);
-                    path.close();
-                }
-                break;
+    private void init(Context context, AttributeSet attrs) {
+        if (attrs != null) {
+            final TypedArray attributes = context.obtainStyledAttributes(attrs, R.styleable.ArcLayout);
+            arcHeight = attributes.getDimensionPixelSize(R.styleable.ArcLayout_arc_height, arcHeight);
+            arcPosition = attributes.getInteger(R.styleable.ArcLayout_arc_position, arcPosition);
+            cropDirection = attributes.getInteger(R.styleable.ArcLayout_arc_cropDirection, cropDirection);
+            attributes.recycle();
         }
+        super.setClipPathCreator(new ClipPathManager.ClipPathCreator() {
+            @Override
+            public Path createClipPath(int width, int height) {
+                final Path path = new Path();
 
-        return path;
-    }
+                final boolean isCropInside = cropDirection == CROP_INSIDE;
 
-    @Override
-    protected void onLayout(boolean changed, int left, int top, int right, int bottom) {
-        super.onLayout(changed, left, top, right, bottom);
-        if (changed) {
-            calculateLayout();
-        }
-    }
-
-    private void calculateLayout() {
-        if (settings == null) {
-            return;
-        }
-        height = getMeasuredHeight();
-        width = getMeasuredWidth();
-        if (width > 0 && height > 0) {
-
-            clipPath = createClipPath();
-            ViewCompat.setElevation(this, settings.getElevation());
-            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP && !settings.isCropInside()) {
-                ViewCompat.setElevation(this, settings.getElevation());
-                setOutlineProvider(new ViewOutlineProvider() {
-                    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
-                    @Override
-                    public void getOutline(View view, Outline outline) {
-                        outline.setConvexPath(clipPath);
+                switch (arcPosition) {
+                    case POSITION_BOTTOM: {
+                        if (isCropInside) {
+                            path.moveTo(0, 0);
+                            path.lineTo(0, height);
+                            path.quadTo(width / 2, height - 2 * arcHeight, width, height);
+                            path.lineTo(width, 0);
+                            path.close();
+                        } else {
+                            path.moveTo(0, 0);
+                            path.lineTo(0, height - arcHeight);
+                            path.quadTo(width / 2, height + arcHeight, width, height - arcHeight);
+                            path.lineTo(width, 0);
+                            path.close();
+                        }
+                        break;
                     }
-                });
+                    case POSITION_TOP:
+                        if (isCropInside) {
+                            path.moveTo(0, height);
+                            path.lineTo(0, 0);
+                            path.quadTo(width / 2, 2 * arcHeight, width, 0);
+                            path.lineTo(width, height);
+                            path.close();
+                        } else {
+                            path.moveTo(0, arcHeight);
+                            path.quadTo(width / 2, -arcHeight, width, arcHeight);
+                            path.lineTo(width, height);
+                            path.lineTo(0, height);
+                            path.close();
+                        }
+                        break;
+                    case POSITION_LEFT:
+                        if (isCropInside) {
+                            path.moveTo(width, 0);
+                            path.lineTo(0, 0);
+                            path.quadTo(arcHeight * 2, height / 2, 0, height);
+                            path.lineTo(width, height);
+                            path.close();
+                        } else {
+                            path.moveTo(width, 0);
+                            path.lineTo(arcHeight, 0);
+                            path.quadTo(-arcHeight, height / 2, arcHeight, height);
+                            path.lineTo(width, height);
+                            path.close();
+                        }
+                        break;
+                    case POSITION_RIGHT:
+                        if (isCropInside) {
+                            path.moveTo(0, 0);
+                            path.lineTo(width, 0);
+                            path.quadTo(width - arcHeight * 2, height / 2, width, height);
+                            path.lineTo(0, height);
+                            path.close();
+                        } else {
+                            path.moveTo(0, 0);
+                            path.lineTo(width - arcHeight, 0);
+                            path.quadTo(width + arcHeight, height / 2, width - arcHeight, height);
+                            path.lineTo(0, height);
+                            path.close();
+                        }
+                        break;
+
+                }
+                return path;
             }
-        }
+
+            @Override
+            public boolean requiresBitmap() {
+                return false;
+            }
+        });
     }
 
 
-    @Override
-    protected void dispatchDraw(Canvas canvas) {
-    	 Paint paint = new Paint();
-        paint.setAntiAlias(true);
-        paint.setColor(Color.WHITE);
-        int saveCount = canvas.saveLayer(0, 0, getWidth(), getHeight(), null, Canvas.ALL_SAVE_FLAG);
-        super.dispatchDraw(canvas);
-        paint.setXfermode(new PorterDuffXfermode(PorterDuff.Mode.MULTIPLY));
-        canvas.drawPath(clipPath, paint);
-        canvas.restoreToCount(saveCount);
-        paint.setXfermode(null);
+    public int getArcPosition() {
+        return arcPosition;
+    }
+
+    public void setArcPosition(@ArcPosition int arcPosition) {
+        this.arcPosition = arcPosition;
+        requiresShapeUpdate();
+    }
+
+    public int getCropDirection() {
+        return cropDirection;
+    }
+
+    public void setCropDirection(@CropDirection int cropDirection) {
+        this.cropDirection = cropDirection;
+        requiresShapeUpdate();
+    }
+
+    public int getArcHeight() {
+        return arcHeight;
+    }
+
+    public void setArcHeight(int arcHeight) {
+        this.arcHeight = arcHeight;
+        requiresShapeUpdate();
+    }
+
+    @IntDef(value = {POSITION_BOTTOM, POSITION_TOP, POSITION_LEFT, POSITION_RIGHT})
+    public @interface ArcPosition {
+    }
+
+    @IntDef(value = {CROP_INSIDE, CROP_OUTSIDE})
+    public @interface CropDirection {
     }
 }
